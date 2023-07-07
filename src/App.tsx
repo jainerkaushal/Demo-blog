8145953639fed9588079e418bd4026a16a74f14e
jainerkaushal <57785235+jainerkaushal@users.noreply.github.com>
2023-07-07 22:20:55 +0000

    my first commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Kaushal <br/>
+      <img src="https://14068d66ba387efac9ce5e4b1741bcf2.r2.cloudflarestorage.com/ai-api/07-23/7f503b24-fbba-43fb-825f-a1e5644312f3_1.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=16b502c87564788383d52ec498a61a24%2F20230707%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230707T213324Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=0cec969b09438b6b114904b08c0742263cd8560fd75fbe638826241e6d615f12" alt="Kaushal Image" />
     </div>
   );
 }
